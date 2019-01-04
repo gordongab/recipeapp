74350f6e66155ab2e02e01a3c86bb3125b26ed7c
Gabriel Gordon <gordongab@gmail.com>
2019-01-03 22:38:42 -0500

    Updates

---
+++ precache-manifest.268d63ade191f25e2d88c7d7a5b92f50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7701815b74baf89f84",
+    "url": "/recipeapp/static/js/runtime~main.af770181.js"
+  },
+  {
+    "revision": "8020ce87665f337badc6",
+    "url": "/recipeapp/static/js/main.8020ce87.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipeapp/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "8020ce87665f337badc6",
+    "url": "/recipeapp/static/css/main.fff0932c.chunk.css"
+  },
+  {
+    "revision": "785628c8c72163a4dff782d9d152fe2b",
+    "url": "/recipeapp/index.html"
+  }
+];
